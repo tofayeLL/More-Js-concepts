4b0945181c315a754177c467486685c5327470d0
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-29 17:36:08 +0600

    finished practice task 2

--- practice.js
+++ practice.js
@@ -11,14 +11,39 @@
 
 //question:1b) Write a function called delayedGreeting() that takes two parameters, name and delay time, and logs a greeting message after the given delay time.
 
-
+/* 
 const displayGreetings = (name, time)=>{
     setTimeout(()=>{
         console.log(`Hello, ${name}!`);
 
     }, time);
 }
-displayGreetings('Alice', 2000);
+displayGreetings('Alice', 2000); */
+
+
+/* -------------------------------------------------------------- */
+
+
+/* --------practice task 2-------- */
+function tellJoke(){
+    console.log("why dont scientist trust atom? because they make me up everything");
+    
+}
+
+   const jokeInterval = setInterval(()=>{
+    tellJoke();
+   
+   }, 2000);
+
+   // After 10 seconds, stop telling jokes
+
+   setTimeout(() => {
+    clearInterval(jokeInterval);
+   }, 11000);
+
+
+    
+  
 
 
 
